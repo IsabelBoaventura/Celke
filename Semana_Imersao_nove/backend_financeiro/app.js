3a10b5b6908b7284a56226daf32fbf459ecdaf1c
Isabel boaventura <boaventuraisabel31@gmail.com>
2021-09-30 14:01:33 -0300

    Add files via upload

--- Semana_Imersao_nove/backend_financeiro/app.js
+++ Semana_Imersao_nove/backend_financeiro/app.js
@@ -1,25 +1,86 @@
 //exportar a dependência 
+//
 const express = require('express');
 
 
+//
 const app = express();
 //quando houver a necessidade de criar rotas, será a const app que será utilizada
 // ou iniciar um servidor 
 
 
-//const db = require('./models/db.js');
+//criar a constante para chamar a funcao que cria a tabela Extrato
+const Extrato = require('./models/Extrato');
+
+
+// para receber os dados em formato JSON
+app.use(express.json());
+
+
+//
+const db = require('./models/db.js');
+const { response } = require('express');
 //excluido daqui, pois o 'Extrato.js' agora será o responsável por trabalhar com o banco 
  
-app.get('/listar', (req, res) => {
-    // '/listar' é uma rota que esta recebendo uma funcao 
-    // modelo da funcao será  'Arrow functions'
+app.get('/listar', async (req, res) => {
+  // '/listar' é uma rota que esta recebendo uma funcao 
+  // modelo da funcao será  'Arrow functions'
 
-  res.send("Extrato Financeiro teste 10/09/2021 ");
+  //Esta mensagem fixa não ira aparecer mais 
+  //res.send("Extrato Financeiro teste 29/09/2021 ");
+  //aogra é para aparecer o que estiverno banco de dados:
+
+  await Extrato.findAll({order: [['id', 'DESC']]})
+    .then(function( extratos){
+      return res.json({
+        erro: false,
+        extratos
+      });
+    })
+    .catch(function(){
+      return res.json({
+        erro: true,
+        mensagem: "Erro: Nenhum lancamento encotrado! "
+      });
+    });
+
+  //findAll funcao para buscar todos os registros do banco, funcao do sequelize
+
+
+ 
   //o que aparecerá na tela , quando esta rota for chamada 
 });
 
 
-//iniciar o projeto, dentro do listen teráa porta  
+//cadastrar um registro
+//req => requisicao 
+//res => resposta 
+app.post('/cadastrar', async ( req, res )=>{
+
+  //para gravar as informações no banco de dados 
+  //aplicando a create ja podemos excluir as outras funcoes 
+  await Extrato.create(req.body)
+    .then( function(){
+      return res.json({
+        erro:false,
+        mensagem: "Lancamento Cadastrado com Sucesso!"
+      });
+    })
+    .catch( function(){
+      return res.status( 400 ).json({
+        erro: true,
+        mensagem:   "Cadastro de Lancamento com ERRO!"
+      });
+    });
+
+  //tratar o recebimento dos dados //req == requisicao
+  //console.log(req.body);
+
+  //res.send("Cadastrar");
+});
+
+
+//iniciar o projeto, dentro do listen terá a porta  
 //app.listen(8080);
 
 
