054c9ebe3f7bf63489dcdf236b7175d8c4ee2b65
Isabel boaventura <boaventuraisabel31@gmail.com>
2021-10-20 13:58:15 -0300

    Add files via upload

--- Semana_Imersao_nove/backend_financeiro/app.js
+++ Semana_Imersao_nove/backend_financeiro/app.js
@@ -1,12 +1,19 @@
 //exportar a dependência 
 //
-const express = require('express');
+
 
 
 //
 const app = express();
 //quando houver a necessidade de criar rotas, será a const app que será utilizada
 // ou iniciar um servidor 
+
+//para adicionar os operadores no sql
+const { Op } = require('sequelize');
+
+//Permitir acesso na API
+const cors = require('cors');
+const express = require('express');
 
 
 //criar a constante para chamar a funcao que cria a tabela Extrato
@@ -15,6 +22,14 @@ const Extrato = require('./models/Extrato');
 
 // para receber os dados em formato JSON
 app.use(express.json());
+app.use((req, res, next) =>{
+  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Headers", "X-PINGOTHER, Content-Type, Authorization");
+  app.use(cors());
+  next();
+});
+
 
 
 //
@@ -22,15 +37,108 @@ const db = require('./models/db.js');
 const { response } = require('express');
 //excluido daqui, pois o 'Extrato.js' agora será o responsável por trabalhar com o banco 
  
-app.get('/listar', async (req, res) => {
+app.get('/listar/:mes/:ano', async (req, res) => {
   // '/listar' é uma rota que esta recebendo uma funcao 
   // modelo da funcao será  'Arrow functions'
+
+  var mesRecebido = new Number(req.params.mes);
+  var anoRecebido = new Number(req.params.ano);
+
+  //console.log(" Mes: " + mesRecebido );
+  //console.log(" Ano: " + anoRecebido );
+
+  const date = new Date(anoRecebido + "-" + mesRecebido);
+  var primeiroDia = new Date(date.getFullYear(), date.getMonth(), 1);//recupera primeiro dia
+  var ultimoDia   = new Date(date.getFullYear(), date.getMonth()+1 , 0 );
+  //console.log(  date.getMonth() );
+  //console.log ( "Primeiro: "+  primeiroDia + " Ultimo " +  ultimoDia );
+
+
+  //var primeiroDia2 = new Date(anoRecebido, mesRecebido-1, 1);
+  //var ultimoDia2   = new Date(anoRecebido, mesRecebido, 0);
+  //console.log ( "Segundo teste >>> Primeiro: "+  primeiroDia2 + " Ultimo " +  ultimoDia2 );
+
 
   //Esta mensagem fixa não ira aparecer mais 
   //res.send("Extrato Financeiro teste 29/09/2021 ");
   //aogra é para aparecer o que estiverno banco de dados:
 
-  await Extrato.findAll({order: [['id', 'DESC']]})
+  /** Esta função tras as informações ordenado pelo ID
+   * Agora será pela data de Vencimento
+   */
+
+  const lancamentos = await Extrato.findAll({
+      order: [['dataVencimento', 'ASC']],
+      where: {
+        "dataVencimento":{
+          [Op.between]:[ primeiroDia, ultimoDia ],
+        }
+      }
+    })
+
+  const valorAPagar  = await Extrato.sum('valor', {
+      //condição
+      where:{
+        tipo: '1',
+        situacao: '2', 
+        "dataVencimento":{
+          [Op.between]:[ primeiroDia, ultimoDia ],
+        }
+      }
+  });
+
+  const valorAReceber   = await Extrato.sum('valor', {
+    //condição
+    where:{
+      tipo: '2',
+      situacao: '1', 
+      "dataVencimento":{
+        [Op.between]:[ primeiroDia, ultimoDia ],
+      }
+    }
+  });
+
+  const valorPago  = await Extrato.sum('valor', {
+    //condição
+    where:{
+      tipo: '1',
+      situacao: '1',
+      "dataVencimento":{
+        [Op.between]:[ primeiroDia, ultimoDia ],
+      }
+    }
+  });
+
+  const valorRecebido   = await Extrato.sum('valor', {
+    //condição
+    where:{
+      tipo: '2',
+      situacao: '1',
+      "dataVencimento":{
+        [Op.between]:[ primeiroDia, ultimoDia ],
+      }
+    }
+  });
+
+  const deve = new Number( valorPago ) - new Number(   valorAPagar  )  ;
+
+  const saldo = new Number(  valorRecebido ) - deve  ;
+  // o que tenho  menos o que devo
+
+    return res.json({
+      erro: false,
+      lancamentos,
+      valorAPagar,
+      valorAReceber,
+      valorPago ,
+      valorRecebido , 
+      deve,
+      saldo  
+    });
+
+    /*
+
+    // order: [['id', 'DESC']]}) //buscar pelo ID
     .then(function( extratos){
       return res.json({
         erro: false,
@@ -43,6 +151,8 @@ app.get('/listar', async (req, res) => {
         mensagem: "Erro: Nenhum lancamento encotrado! "
       });
     });
+
+    */
 
   //findAll funcao para buscar todos os registros do banco, funcao do sequelize
 
